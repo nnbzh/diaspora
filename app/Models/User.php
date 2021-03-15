1e63c9f48c26d624f9ad2786e21bf07ab674cbad
Samat Begimbay <deathprodram@gmail.com>
2021-03-15 13:51:18 +0500

    Add laravel passport

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,17 +6,15 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+
 use App\Models\RequestModel;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    
     protected $fillable = [
         'surname',
         'username',
